42e558255034323a983633df4d1a7ccfc373213a
Naqash-Muhammad <92191163+Naqash-Muhammad@users.noreply.github.com>
2022-01-11 11:10:21 -0700

    LastApi

---
+++ EndApi.js
@@ -0,0 +1,35 @@
+const Api = [
+    {
+        id : "0",
+        Name : "Naqash",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+    {
+        id : "1",
+        Name : "ahead",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+
+    {
+        id : "2",
+        Name : "banti",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+
+    {
+        id : "3",
+        Name : "Sani",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+
+    {
+        id : "4",
+        Name : "Taha",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+]
